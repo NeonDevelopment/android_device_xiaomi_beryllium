8f7394d1d3fa37a6192f967463982a526c366e24
Amit Shekhar <ashekhar@codeaurora.org>
2017-04-03 15:49:55 -0700

    beryllium: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    mediacodec: allow getdents64, sysinfo and getcwd
    
    Vendor extended policy files are separated from the aosp
    and will now reside in /vendor
    
    Change-Id: If59d04b36d744efb74209ae36e76dd16cb18f606
    CRs-Fixed: 2027600
    
    beryllium: Add system call to the SE whitelist
    
    Add system call to the SE policy whitelist
    
    Change-Id: I1793d20c49f838929aa0dc1720eb062535ae8f09
    CRs-Fixed: 2050577
    
    beryllium: mediacodec: Allow syscall for VPP
    
    Add inotify_intl, inotify_add_watch and inotify_rm_watch for VPP.
    
    CRs-Fixed: 2058795
    Change-Id: I294b03f4a46020a022cacf1267ebf68b0bdd6e19
    
    beryllium: mediacodec: Allow syscalls needed by OMX component
    
    perf daemon needs uname, ueventd, timer_create,
    timer_settime, rt_sigtimedwait syscalls. Add them
    to media.codec's whitelist.
    
    CRs-Fixed: 2062950
    Change-Id: Ib07d73140470d9def0d9411469ee1525e13f4bc7

--- device.mk
+++ device.mk
@@ -276,6 +276,10 @@ PRODUCT_PACKAGES += \
     RcsService \
     PresencePolling
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
