210308c9991dd8aa7b4c96e2a4b65eaf71ce98a8
TheScarastic <warabhishek@gmail.com>
2018-10-02 10:23:32 -0400

    beryllium: Set default display density
     * For some reason, this isn't set in vendor image.
    
    Change-Id: I7e63a35e8a96172c02af0392f62c58835ab7a994

--- device-props.mk
+++ device-props.mk
@@ -29,7 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
     ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
     ro.qcom.ad.sensortype=2 \
-    ro.qualcomm.cabl=0
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=480
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
