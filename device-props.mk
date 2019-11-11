25df7d7158382cfb2600d21bb801851115aae4e4
Bruno Martins <bgcngm@gmail.com>
2019-11-11 23:13:51 +0000

    beryllium: Drop QC location support
    This reverts commit 4742d474370d5b1cfe300d61ebe19a6496a545b7.
    
    It leaks the IP, location and more to some 3rd party companies,
    bla bla bla. Let's stop with all the whining.
    
    Change-Id: I77a82306b21030056e30d43eda4e504963554309

--- device-props.mk
+++ device-props.mk
@@ -107,10 +107,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.hw.fm.init=0
 
-# GNSS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.overlay.izat.optin=rro
-
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
